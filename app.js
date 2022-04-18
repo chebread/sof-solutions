e8d581c9ba29d608231492f4415480753223e857
Cha Haneum <developer_haneum_11@icloud.com>
2022-04-19 06:05:48 +0900

    12 month

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 // empty
+for (let i = 0; i < 13; i++) {
+  let month = new Date().getMonth() + 1 + i;
+  if (month > 12) {
+    month = month - 12;
+  }
+  const nameMonth = {
+    1: "Jan",
+    2: "Feb",
+    3: "Mar",
+    4: "Apr",
+    5: "May",
+    6: "Jun",
+    7: "Jul",
+    8: "Aug",
+    9: "Sep",
+    10: "Oct",
+    11: "Nov",
+    12: "Dec",
+  };
+  console.log(
+    new Date().getFullYear() + " Year " + nameMonth[month] + " Month "
+  );
+}
